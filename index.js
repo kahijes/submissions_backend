74e37d781140957c64bc0a859b5636b687cdd114
kahijes <j.kahilainen@live.fi>
2021-05-04 14:51:17 +0300

    xd

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var morgan = require('morgan')
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token('postxd', function (req, res) {return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postxd'))
 
